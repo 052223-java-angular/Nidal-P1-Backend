0ad31ca96bd6e04f7a0154b6f1709211ac6593a0
Nidal Alafandi <niafandi@gmail.com>
2023-06-05 10:18:57 -0400

    ERD done

---
+++ src/main/java/com/revature/goodPet/GoodPetApplication.java
@@ -0,0 +1,13 @@
+package com.revature.goodPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/goodPet/GoodPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.goodPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
